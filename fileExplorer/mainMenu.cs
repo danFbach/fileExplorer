cf472cc92cb988db5bf8ada464702e59a35ebb2f
danFbach <quiktekrepair@gmail.com>
2017-02-02 03:30:10 -0600

    small tweaks

--- fileExplorer/mainMenu.cs
+++ fileExplorer/mainMenu.cs
@@ -63,5 +63,44 @@ namespace fileExplorer
             return directory;
         }
         #endregion setupFuncs
+
+        #region renamingUtil
+
+
+        public void filter()
+        {
+            string dir = p.rl(p.br + "Enter directory for files to be renamed. (Replacing periods with spaces.)" + p.br + "Directory: ", p.wht, p.grn);
+            string[] _filePackRAW = Directory.GetFiles(dir);
+            List<FileInfo> _filePack = new List<FileInfo>();
+            List<FileInfo> _newFilePack = new List<FileInfo>();
+            foreach (string f in _filePackRAW)
+            {
+                if (File.Exists(f))
+                {
+                    _filePack.Add(new FileInfo(f));
+                }
+            }
+            string seasonNum = _filePack[0].DirectoryName.Split('\\').Last().Split(' ').Last();
+            for (int i = 0; i < _filePack.Count(); i++)
+            {
+                string[] fBrk = _filePack[i].Name.Split('.');
+                string cleanName = "Psych " + _filePack[i].Name;
+
+
+                //cleanName += "Psych S";
+                //if(Convert.ToInt16(seasonNum) < 10) { cleanName += "0"; }
+                //cleanName += seasonNum + "E";
+                //string oldName = _filePack[i].Name.Split('.').First();
+                //string epNum = oldName.Split(' ').Last();
+                //if(Convert.ToInt16(epNum) < 10) { cleanName += "0"; }
+                //cleanName += epNum;
+                //cleanName += "." + fBrk.Last();
+                File.Move(_filePack[i].FullName, _filePack[i].Directory + "\\" + cleanName);
+            }
+            p.rk("pausing, press any key to continue.", p.grn, p.blk);
+            filter();
+        }
+
+        #endregion renamingUtil
     }
 }
